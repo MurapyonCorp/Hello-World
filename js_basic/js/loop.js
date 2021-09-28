09933afd786f96293a549f181ff17effc39e4caa
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-28 21:14:47 +0900

    do while

--- js_basic/js/loop.js
+++ js_basic/js/loop.js
@@ -15,15 +15,21 @@ for (var i = 1; i <= 3; i++) {
 // 例題 while
 // 変数 jの値を1から3まで出力するプログラムを、
 // while文を使って記述
+/*
 var j = 1;
 while(j <= 3) {
   console.log(j);
   j++;
 }
-
+*/
 
 
 // 例題 do while
 // 例題
 // 変数kの値を1から3までコンソールに出力するプログラムを、
 // do while文を使って記述する
+var k = 1;
+do {
+  console.log(k);
+  k++;
+} while (k <= 3);
