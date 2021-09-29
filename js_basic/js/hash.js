6dd6f6f5221bfc2431d53f99a57dc3008fdf8e64
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-29 16:47:54 +0900

    hash

--- js_basic/js/hash.js
+++ js_basic/js/hash.js
@@ -8,6 +8,11 @@
 // gender : 女性
 // birth : 1990/01/01
 // それぞれの値をキーを指定して取り出して、コンソールに表示。
+var user = {name: '山田花子', gender: '女性', birth: '1990/1/1'};
+console.log(user.name);
+console.log(user.gender);
+console.log(user.birth);
 
 
 // 補足：範囲外指定
+console.log(user.hoge);
