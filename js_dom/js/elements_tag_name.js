8672e505f6a1df2c4886cadfdcddd43e2a580aaf
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-03 16:18:54 +0900

    get_elememt_by_tagname

--- js_dom/js/elements_tag_name.js
+++ js_dom/js/elements_tag_name.js
@@ -1,1 +1,7 @@
-// タグ名がdivの要素を取得し、その内容（テキスト）をコンソールに出力する。z
+// タグ名がdivの要素を取得し、その内容（テキスト）をコンソールに出力する。
+function showElements() {
+  let elements = document.getElementsByTagName('div');
+  for (let i = 0; i < elements.length; i++) {
+    console.log(elements[i].innerText);
+  }
+}
