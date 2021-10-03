f478a8dce07e075b76c85f08d5e02aeb0187aeea
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-03 16:00:40 +0900

    getelementbyid

--- js_dom/js/element_id.js
+++ js_dom/js/element_id.js
@@ -3,3 +3,9 @@
 
 // 現在のid:resultの内容（テキスト）をコンソールに表示。
 // id:resultの内容（テキスト）を、現在時刻（日時）に書き換え。
+function show() {
+  let result = document.getElementById('result');
+  console.log(result.innerText);
+  let nowDate = new Date();
+  result.innerText = nowDate.toLocaleString();
+}
