0216f263af2c70473403f7adfc9c050d82214840
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-04 17:09:07 +0900

    append_child

--- js_dom/js/append_child.js
+++ js_dom/js/append_child.js
@@ -4,11 +4,12 @@
 // 追加される文字列は”追加文字列”とする。
 function append() {
   // li要素を生成
-
+  let li = document.createElement('li');
   // テキストノードを生成
-
+  let text = document.createTextNode('追加文字列');
   // liタグの要素に、テキストノード textを追加
-
+  li.appendChild(text);
   // idがlistsのulタグに、liを追加。具体的には<li>追加文字列</li>が、追加される。
-  
+  let listsElement = document.getElementById('lists');
+  listsElement.appendChild(li);
 }
