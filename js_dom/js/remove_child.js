03ab6fdfd3e3bf5dd6c71cfbc3e76c828c0418c8
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-04 17:29:41 +0900

    remove_child

--- js_dom/js/remove_child.js
+++ js_dom/js/remove_child.js
@@ -1,3 +1,9 @@
 // リストとボタンを設置
 // ボタンをクリックすると
 // リストの子要素が最後のものから削除される
+function remove() {
+  let parentElement = document.getElementById('lists');
+  let elements = parentElement.getElementsByTagName('li');
+  let removeIndex = elements.length -1;
+  parentElement.removeChild(elements[removeIndex]);
+}
