12ffd6db982a9837bb7732c2771f3134f1229ea6
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-22 10:36:55 +0900

    logical

--- book-js/practice/3-5_logical/js/logical.js
+++ book-js/practice/3-5_logical/js/logical.js
@@ -1,1 +1,11 @@
 'use strict';
+let message;
+let hour = new Date().getHours();
+if (hour >= 19 && hour < 21) {
+  message = 'お弁当30％OFF!';
+} else if (hour === 9 || hour === 15) {
+  message = 'お弁当1個買ったら1個おまけ！';
+} else {
+  message = 'お弁当はいかがですか？';
+}
+window.alert(message);
