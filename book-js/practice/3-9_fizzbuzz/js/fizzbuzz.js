d66f087f4fc52124cdd3adb65d50dfe0d978e6e5
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-11-03 11:39:37 +0900

    fizzbuzz

--- book-js/practice/3-9_fizzbuzz/js/fizzbuzz.js
+++ book-js/practice/3-9_fizzbuzz/js/fizzbuzz.js
@@ -1,1 +1,18 @@
 'use strict';
+function fizzbuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz!';
+  } else if (num % 3 === 0) {
+    return 'Fizz!';
+  } else if (num % 5 === 0) {
+    return 'Buzz!';
+  } else {
+    return num;
+  }
+}
+
+let i = 1;
+while (i <= 30) {
+  console.log(fizzbuzz(i));
+  i += 1;
+}
