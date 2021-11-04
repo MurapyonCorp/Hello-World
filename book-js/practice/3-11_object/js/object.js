a7a0fe0f2ef0af5fefdbe941911dbb6a3b2d765e
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-11-04 21:46:09 +0900

    object

--- book-js/practice/3-11_object/js/object.js
+++ book-js/practice/3-11_object/js/object.js
@@ -1,1 +1,7 @@
 'use strict';
+let jsbook = {title: 'JavaScript入門', price: 700, stock: 5};
+jsbook.stock = 10;
+
+document.getElementById('title').textContent = jsbook.title;
+document.getElementById('price').textContent = jsbook.price + '円';
+document.getElementById('stock').textContent = jsbook.stock;
