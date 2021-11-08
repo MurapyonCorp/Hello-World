87db8e00d3ef022a3b22002e181d438233fd2697
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2021-11-08 10:51:34 +0900

    input

--- book-js/practice/4-1_input/js/input.js
+++ book-js/practice/4-1_input/js/input.js
@@ -1,1 +1,6 @@
 'use strict';
+document.getElementById('form').onsubmit = function (event) {
+  event.preventDefault();
+  const search = document.getElementById('form').word.value;
+  document.getElementById('output').textContent = `「${search}」の検索中…`;
+};
