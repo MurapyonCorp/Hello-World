629623f4e3c4d0ad70b01d55ada9cd4a1d409795
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-01-09 23:27:49 +0900

    countdown

--- book-js/practice/5-1_countdown/js/countdown.js
+++ book-js/practice/5-1_countdown/js/countdown.js
@@ -1,1 +1,34 @@
 'use strict';
+function countdown(due) {
+  const now = new Date();
+  const rest = due.getTime() - now.getTime();
+  const sec = Math.floor(rest / 1000) % 60;
+  const min = Math.floor(rest / 1000 / 60) % 60;
+  const hours = Math.floor(rest / 1000 / 60 / 60) % 60;
+  const days = Math.floor(rest / 1000 / 60 / 60 / 60) % 60;
+  const count = [days, hours, min, sec];
+  
+  return count;
+}
+
+const goal = new Date(2025, 4, 3);
+/*goal.setHours(23);
+  goal.getMinutes(59);
+  goal.getSeconds(59);*/
+
+console.log(countdown(goal));
+function recale() {
+  const counter = countdown(goal);
+  // const time = `${counter[1]}時間${counter[2]}分${counter[3]}秒`;
+  // document.getElementById('timer').textContent = time;
+  document.getElementById('day').textContent = counter[0];
+  document.getElementById('hour').textContent = counter[1];
+  document.getElementById('min').textContent = String(counter[2]).padStart(2, '0');
+  document.getElementById('sec').textContent = String(counter[3]).padStart(2, '0');
+  refresh();
+}
+
+function refresh() {
+  setTimeout(recale, 1000);
+}
+recale();
