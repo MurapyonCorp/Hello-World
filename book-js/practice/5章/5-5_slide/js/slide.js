3216d39614970d8780cbecfb266b8678708fc9db
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-02-17 17:47:02 +0900

    5-5 step1

---
+++ book-js/practice/5章/5-5_slide/js/slide.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const images = ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg', 'img/img5.jpg'];
+let current = 0;
+
+function chamgeImage(num) {
+  if (current + num >= 0 && current + num < images.length) {
+    current += num;
+    document.getElementById('main_image').src = images[current];
+  }
+};
+
+document.getElementById('prev').onclick = function () {
+  chamgeImage(-1)
+};
+document.getElementById('next').onclick = function () {
+  chamgeImage(1)
+};
